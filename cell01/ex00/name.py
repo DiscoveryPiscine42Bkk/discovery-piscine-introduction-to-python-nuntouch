d30f2d8a511d72d5cf67a97d2573c3c71e233c77
nuntouch <nuntouch.phe@gmail.com>
2025-05-05 17:36:59 +0700

    ex00

---
+++ cell01/ex00/name.py
@@ -0,0 +1,8 @@
+first_name = "keby"
+last_name = "cat"
+def main(first_name,last_name):
+    first_name = "keby"
+    last_name = "cat"
+    print (f"{first_name} {last_name}")
+
+main (first_name,last_name)
