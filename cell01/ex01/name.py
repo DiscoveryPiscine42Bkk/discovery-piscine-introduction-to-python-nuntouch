23e65c54bfe49815d1e76290c8e661a9c1c49492
nuntouch <nuntouch.phe@gmail.com>
2025-05-05 17:41:56 +0700

    cell01_ex01

---
+++ cell01/ex01/name.py
@@ -0,0 +1,10 @@
+first_name = "keby"
+last_name = "cat"
+whole_name = first_name + " " + last_name
+def main(first_name,last_name,whole_name):
+    first_name = "keby"
+    last_name = "cat"
+    whole_name = first_name + " " + last_name
+    print (whole_name)
+
+main (first_name,last_name,whole_name)
