d6a89c8eaaed777f70df6da818fc898f06f280d1
nuntouch <nuntouch.phe@gmail.com>
2025-05-06 13:23:20 +0700

    ex03

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,8 @@
+first_name = "key"
+last_name = "42"
+txt = f"Hey, what's your first name? : {first_name}"
+txt2 = f"And your last name? : {last_name}"
+txt3 = f"Well, pleased to meet yoy, {first_name} {last_name}"
+print(txt)
+print(txt2)
+print(txt3)
