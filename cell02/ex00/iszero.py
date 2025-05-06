2d068e84ea4a960abb876ca8e1456790852f4f77
nuntouch <nuntouch.phe@gmail.com>
2025-05-06 19:51:50 +0700

    1

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -2,7 +2,7 @@
 
 num = int(input())
 if num == 0:
-    print ("this number is equal to zero.")
+    print ("This number is equal to zero.")
 else :
     print ("This number is different from zero.")
 
